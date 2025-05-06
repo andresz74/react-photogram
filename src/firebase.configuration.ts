a7175d0bbb6eab5aa57f34229469720e96d6cb77
Andres Z <azenteno74@gmail.com>
2025-05-06 19:55:12 -0400

    Revert "Delete src/firebase.configuration.ts (#14)"
    This reverts commit ad802ddc2f4b1d83d5f52300962472a0d24078a0.

---
+++ src/firebase.configuration.ts
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Firebase configuration object
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
+    databaseURL: 'https://reactblogmd.firebaseio.com',
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
+    messagingSenderId: '169824492279',
+    appId: process.env.REACT_APP_FIREBASE_APP_ID!,
+};
+
+// Firestore collections
+export const imagesDbCollection = 'imagesArray';
+export const usersDbCollection = 'usersArray';
+
+// Initialize Firebase app
+const app = !firebase.apps.length 
+    ? firebase.initializeApp(firebaseConfig) 
+    : firebase.app(); // Reuse existing instance
+
+// Firebase authentication setup
+export const auth = firebase.auth();
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(() => console.log('Firebase persistence set to LOCAL'))
+    .catch((error) => console.error('Error setting Firebase persistence:', error));
+
+// Initialize Firestore
+export const db = firebase.firestore(app);
