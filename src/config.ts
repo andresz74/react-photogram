d275121dd249c35275fd02712643e56fe85b4c13
Andres Zenteno <azenteno74@gmail.com>
2025-12-20 18:30:15 -0500

    fix: update local image-api URL

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 const config = {
   apiBaseUrl: isDevelopment
-    ? 'https://192.168.1.181/image-api'  // In development, point to your Nginx server or local deployment (http://localhost:3003)
+    ? 'http://192.168.1.181/image-api'  // In development, point to your Nginx server or local deployment (http://localhost:3003)
     : '/image-api',  // In production, the same /api as Nginx will serve it
 };
 
