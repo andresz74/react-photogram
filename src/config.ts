40dfdcd04edc3639a3c0e9e2e424657850493459
Andres Zenteno <azenteno74@gmail.com>
2025-12-20 18:35:23 -0500

    fix: update api base url

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 const config = {
   apiBaseUrl: isDevelopment
-    ? 'http://192.168.1.181/image-api'  // In development, point to your Nginx server or local deployment (http://localhost:3003)
-    : '/image-api',  // In production, the same /api as Nginx will serve it
+    ? 'http://192.168.1.181:3003'  // In development, point to your Nginx server or local deployment (http://localhost:3003)
+    : '',  // In production, the same /api as Nginx will serve it
 };
 
 export default config;
