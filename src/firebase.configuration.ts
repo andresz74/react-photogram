2e5977decb2392528a6813c46af2a6e850a9c378
Andres Zenteno <azenteno74@gmail.com>
2025-12-21 00:47:33 -0500

    feat: update firebase configuration

--- src/firebase.configuration.ts
+++ src/firebase.configuration.ts
@@ -7,9 +7,10 @@ export const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
-    messagingSenderId: '169824492279',
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
     appId: process.env.REACT_APP_FIREBASE_APP_ID!,
-    measurementId: "G-2D2NQQF8YB"
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Firestore collections
