c824a5f4f9fa451db8c10193bdcc646344d6d4b3
Andres Zenteno <azenteno74@gmail.com>
2025-12-20 21:51:55 -0500

    feat: add API_URL variable

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const config = {
   apiBaseUrl: isDevelopment
     ? 'http://192.168.1.181:3003'  // In development, point to your Nginx server or local deployment (http://localhost:3003)
-    : '',  // In production, the same /api as Nginx will serve it
+    : process.env.API_URL,  // In production, the same /api as Nginx will serve it
 };
 
 export default config;
