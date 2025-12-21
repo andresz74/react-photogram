e4ebbad8e3152d4e9e4c0a359a1b81f3682053de
Andres Z <azenteno74@gmail.com>
2025-12-21 00:53:15 -0500

    Image api url (#24)
    * fix: update local image-api URL
    
    * fix: update api base url
    
    * feat: add API_URL variable
    
    * fix: update docs
    
    * fix: update variables naming
    
    * temp: update apiBaseUrl
    
    * temp: add console.log to images
    
    * temp: comment unused variable
    
    * fix: clean firebase config
    
    * fix: add measurementId
    
    * feat: update firebase configuration
    
    * fix: clean code

--- src/firebase.configuration.ts
+++ src/firebase.configuration.ts
@@ -6,10 +6,11 @@ import 'firebase/firestore';
 export const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
-    databaseURL: 'https://reactblogmd.firebaseio.com',
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
-    messagingSenderId: '169824492279',
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
     appId: process.env.REACT_APP_FIREBASE_APP_ID!,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Firestore collections
